6ceface00e04852de3efa68f21afe181c7330351
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-13 11:15:27 +1000

    Update to Swift 4 package format

--- Package.swift
+++ Package.swift
@@ -1,3 +1,11 @@
+// swift-tools-version:4.0
+
 import PackageDescription 
 
-let package = Package(name: "CCogl")
+let package = Package(name: "CCogl",
+	pkgConfig: "cogl-gl-1.0 cogl-path-1.0 cairo glib-2.0 gio-unix-2.0",
+	providers: [
+		.brew(["cogl", "cairo", "glib", "glib-networking", "gobject-introspection"]),
+		.apt(["libcogl1.0-dev", "libcairo2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+	]
+)
